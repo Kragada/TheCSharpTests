57ce6fd53343a844e468dc355de7e344682423a0
Kragada <35291972+Kragada@users.noreply.github.com>
2021-09-24 16:19:39 +0200

    TestCopy

---
+++ testclass copy.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+namespace TheCSharpTests
+{
+public class testclass{
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(8)]
+    public void MyFirstTheory(int mynumber){
+        Assert.True(Program.IsOdd(mynumber));
+    }
+
+    [Fact]
+    public void PassingAddTest(){
+        Assert.Equal(4, Program.Add(2,2));
+    }
+    [Fact]
+    public void FailingAddTest(){
+        Assert.NotEqual(5, Program.Add(2,2));
+    }
+}
+}
