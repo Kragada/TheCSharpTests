56cbf47a550578e3f3f8262060c6439f8ab855cb
Kragada <35291972+Kragada@users.noreply.github.com>
2021-10-01 17:51:12 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace TheCSharpTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World One More Times");
+            Console.WriteLine("Hello World");
             int yy = Add(4,5);
             Console.WriteLine(yy);
             Console.WriteLine(IsOdd(5));
